6185c87422e55cf75b7c201f9f09858a850ae7a1
Omar ElKhatib <elkhatibomar@outlook.com>
2023-10-27 09:54:45 +0300

    dynamically choose port with command args

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
+type serverConfig struct {
+	port uint
+}
+
 func main() {
+	cfg := serverConfig{}
+
+	flag.UintVar(&cfg.port, "port", 4000, "Specify the port on which the game will run")
+
+	flag.Parse()
+
+	serverLogger := log.New(os.Stdout, "Server: ", log.LstdFlags)
+
+	if err := serve(cfg, serverLogger); err != nil {
+		serverLogger.Fatalf("%v", err)
+	}
+}
+
+func serve(cfg serverConfig, serverLogger *log.Logger) error {
 	fs := http.FileServer(http.Dir("static"))
 
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
@@ -13,5 +35,8 @@ func main() {
 		http.ServeFile(w, r, "game.html")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	address := fmt.Sprintf(":%d", cfg.port)
+
+	serverLogger.Printf("Server is running on port %d\n", cfg.port)
+	return http.ListenAndServe(address, nil)
 }
